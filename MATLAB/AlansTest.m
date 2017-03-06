ed40eaf40928277592b4690bf77c5a27c90bfee3
alannguyen5102 <aknguyen@scu.edu>
2017-03-06 15:09:44 -0800

    Add files via upload
    These files can send data to data turbine

---
+++ MATLAB/AlansTest.m
@@ -0,0 +1,38 @@
+function self=AlansTest()
+import com.rbnb.sapi.*;
+
+robotName = 'Derp';
+ipAddress = 'localhost';
+tcpPort = '3333';
+self.alanssend = @alanssend;
+
+source=Source(1,'none',0);
+source.OpenRBNBConnection(strcat(ipAddress,':',tcpPort),robotName);
+cmap=ChannelMap;
+cmap.Add(robotName);
+source.Register(cmap);
+%while true
+%    cmap=ChannelMap;
+%    cmap.Add(robotName);
+%    cmap.PutTimeAuto('timeofday');
+%    cmap.PutDataAsByteArray(0,uint8(inputVector));
+    
+%    source.Flush(cmap,true);
+%    pause(1);
+%end
+
+    function alanssend( inputVector )
+        inputVector = int8(inputVector);
+        disp(inputVector);
+    %UNTITLED2 Summary of this function goes here
+    %   Detailed explanation goes here
+        import com.rbnb.sapi.*;
+        cmap=ChannelMap;
+        cmap.Add(robotName);
+        cmap.PutTimeAuto('timeofday');
+        %cmap.PutDataAsFloat32(0,uint8(inputVector));
+        cmap.PutDataAsByteArray(0, inputVector);
+        source.Flush(cmap,true);
+
+    end
+end

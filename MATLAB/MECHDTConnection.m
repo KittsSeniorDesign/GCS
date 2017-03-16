a55e8b3defd081a352c02b4e8cdce40b01b71df9
Pillager225 <Pillager225@users.noreply.github.com>
2017-03-15 17:17:59 -0700

    ey command packets

--- MATLAB/MECHDTConnection.m
+++ MATLAB/MECHDTConnection.m
@@ -1,29 +1,31 @@
-function self=RobotDTConnection(robotName,ipAddress,tcpPort)
+function self=MECHDTConnection(robotName,ipAddress,tcpPort)
 
 self.MECH_send = @MECH_send;
 connect = controller(ipAddress,tcpPort,'matlab');
 funct = registerfunction(connect,'MECHDataParser','*/MECHdata');
 channelidx = addcommandchannel(connect,'commands');
 start(connect);
-
-source=Source(1,'none',0);
-source.OpenRBNBConnection(strcat(ipAddress,':',tcpPort),robotName);
-cmap=ChannelMap;
-cmap.Add(robotName);
-
-source.Register(cmap);
-
+scrdstid = 1;
+controlschem = 2;
+function funct = datamap(inp, in_min, in_max, out_min, out_max)
+    funct = (inp - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+end
 function MECH_send( inputVector )
-    inputVector = int8(inputVector);
-    inputVector = arrayfun(datamap, inputVector, -256, 256, 0, 2048);
-    disp(inputVector);
+    
+    
+    inputVector = uint16([datamap(inputVector(1), -1.0, 1.0, 0.0, 2048.0) datamap(inputVector(2), -1.0, 1.0, 0.0, 2048.0)]);
 
-    import com.rbnb.sapi.*;
-    cmap=ChannelMap;
-    cmap.Add(robotName);
-    cmap.PutTimeAuto('timeofday');
-    %cmap.PutDataAsFloat32(0,uint8(inputVector));
-    cmap.PutDataAsByteArray(0, inputVector);
-    source.Flush(cmap,true);
+
+    topleft = uint8(bitshift(inputVector(1), -8));
+    botleft = uint8(bitshift(bitshift(inputVector(1), 8), -8));
+  
+    topright = uint8(bitshift(inputVector(2), -8));
+    botright = uint8(bitshift(bitshift(inputVector(2), 8), -8));
+
+    %disp(inputVector);
+    A = uint8([scrdstid controlschem topleft botleft topright botright]);
+    disp(A);
+    sendcommand(connect, channelidx, A);
+end
 
 end
